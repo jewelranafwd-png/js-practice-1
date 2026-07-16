cd2944ac3fcd2b87a3fe1f5d683532788ba9f9ce
Jewel Rana <jewelranafwd@gmail.com>
2026-07-16 16:26:32 +0600

    task7

---
+++ task7.js
@@ -0,0 +1,15 @@
+// const carSpeed = 79;
+
+// if (carSpeed>80){
+//     console.log('Overspeeding');
+// }else{
+//     console.log('Normal Speed');
+// }
+
+const carSpeed = 89;
+
+if (carSpeed>80){
+    console.log('Overspeeding');
+}else{
+    console.log('Normal Speed');
+}
