781cfdcb42338f896b21f54ac784d0f146fab853
Jewel Rana <jewelranafwd@gmail.com>
2026-07-16 15:20:53 +0600

    task4

---
+++ task4.js
@@ -0,0 +1,7 @@
+// let umbrella = 0;
+
+// (umbrella == 0) ? console.log("out of stock") : console.log("we have lot of umbrella");
+
+let umbrella = 55;
+
+(umbrella == 0) ? console.log("out of stock") : console.log("we have lot of umbrella");
