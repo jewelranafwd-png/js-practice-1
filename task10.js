e97260b2f988973294740d7bba23d0146389881f
Jewel Rana <jewelranafwd@gmail.com>
2026-07-16 17:04:42 +0600

    task10

---
+++ task10.js
@@ -0,0 +1,17 @@
+// const userName = 'edmin';
+// const passWord = 1234;
+
+// if (userName=='edmin' && passWord==1234){
+//     console.log('Login successful');
+// }else{
+//     console.log('login failed');
+// }
+
+const userName = 'rdmin';
+const passWord = 1234;
+
+if (userName=='edmin' && passWord==1234){
+    console.log('Login successful');
+}else{
+    console.log('login failed');
+}
