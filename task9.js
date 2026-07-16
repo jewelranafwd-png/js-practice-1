4649eabfaa502cc6ed6a32695bb0e69b7fb45a82
Jewel Rana <jewelranafwd@gmail.com>
2026-07-16 16:49:29 +0600

    task9

---
+++ task9.js
@@ -0,0 +1,26 @@
+// const isWeekend = true;
+// const isHoliday = true;
+
+// if (isHoliday || isWeekend){
+//     console.log('No work today');
+// }else{
+//     console.log('go to work');
+// }
+
+// const isWeekend = true;
+// const isHoliday = false;
+
+// if (isHoliday || isWeekend){
+//     console.log('No work today');
+// }else{
+//     console.log('go to work');
+// }
+
+const isWeekend = false;
+const isHoliday = false;
+
+if (isHoliday || isWeekend){
+    console.log('No work today');
+}else{
+    console.log('go to work');
+}
