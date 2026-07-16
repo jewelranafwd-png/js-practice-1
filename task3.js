47812de494b8c6dff7099d7f2c99ebe32377955e
Jewel Rana <jewelranafwd@gmail.com>
2026-07-16 15:14:04 +0600

    task3

---
+++ task3.js
@@ -0,0 +1,11 @@
+const isRainig = false;
+if (isRainig){
+    console.log("Take the umbrella");
+}else{
+    console.log("khelte ja")
+}
+// if (!isRainig){
+//     console.log("Take the umbrella");
+// }else{
+//     console.log("khelte ja")
+// }
