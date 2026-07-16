3c1b5c3ad29ceff884823802068b57d7e758ada1
Jewel Rana <jewelranafwd@gmail.com>
2026-07-16 17:15:12 +0600

    task11

---
+++ task11.js
@@ -0,0 +1,25 @@
+// const mark = 80;
+// if (mark >=80){
+//     console.log('A+');
+// }else if(mark >=70){
+//     console.log('A');
+// }else if(mark >=60) {
+//     console.log('B');
+// }else if(mark >=50){
+//     console.log('C');
+// }else{
+//     console.log('F');
+// }
+const mark = 40;
+if (mark >=80){
+    console.log('A+');
+}else if(mark >=70){
+    console.log('A');
+}else if(mark >=60) {
+    console.log('B');
+}else if(mark >=50){
+    console.log('C');
+}else{
+    console.log('F');
+}
+
