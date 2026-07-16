b4fe6f39f2381f73707dc0e7f95d029f7cfc1d6a
Jewel Rana <jewelranafwd@gmail.com>
2026-07-16 16:41:37 +0600

    task8

---
+++ task8.js
@@ -0,0 +1,17 @@
+// const age = 20;
+// const hasTicket = true;
+
+// if (hasTicket && age>= 18){
+//     console.log('Entry Allowed');
+// }else{
+//     console.log('You r not welcome');
+// }
+
+const age = 7;
+const hasTicket = true;
+
+if (hasTicket && age>= 18){
+    console.log('Entry Allowed');
+}else{
+    console.log('You r not welcome');
+}
