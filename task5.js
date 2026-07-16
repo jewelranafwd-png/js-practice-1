857cf278e36ab251450daf4278ee7ffab362e113
Jewel Rana <jewelranafwd@gmail.com>
2026-07-16 15:32:24 +0600

    task5

---
+++ task5.js
@@ -0,0 +1,8 @@
+let number = 700;
+if (number>0) {
+    console.log("number is positive");
+}else if(number<0) {
+    console.log("number is Negative");
+}else{
+    console.log("number is neutral");
+}
