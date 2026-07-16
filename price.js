692ac1a68b9335ab916ae5d74aa3edcd25fb787b
Jewel Rana <jewelranafwd@gmail.com>
2026-07-16 14:58:09 +0600

    first task

---
+++ price.js
@@ -0,0 +1,11 @@
+const price1 = 10;
+const price2 = 20;
+
+// console.log(price1==price2);
+// console.log(price1===price2);
+// console.log(price1!=price2);
+// console.log(price1!==price2);
+// console.log(price1>price2);
+// console.log(price1<price2);
+// console.log(price1>=price2);
+console.log(price1<=price2);
