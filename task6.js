5c78ad7cdd71c2acc77789db8f051c2a3ed9599f
Jewel Rana <jewelranafwd@gmail.com>
2026-07-16 16:16:42 +0600

    task6

---
+++ task6.js
@@ -0,0 +1,12 @@
+let year=2011;
+if (year%4==0) {
+    console.log(year + ' is a leap year');
+}else{
+    console.log(year+' isnt a leapyear');
+}
+const year2=2026;
+if (year2%4==0){
+    console.log(year2 + ' is a leap year');
+}else{
+    console.log(year2+' isnt a leapyear');
+}
